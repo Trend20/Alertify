6a4687ccacde88928f439d66ed9e7f8d56898043
Trend20 <enockomondi305@gmail.com>
2022-10-15 08:48:50 +0300

    adding app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FeaturesComponent,
+    PricingComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
