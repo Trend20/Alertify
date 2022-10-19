cc8cf9e761c4e625a6bb62cf04f73d61c1d42bd7
Trend20 <enockomondi305@gmail.com>
2022-10-19 15:22:57 +0300

    features section

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturesComponent implements OnInit {
 
+  featuresContent=[
+    {
+      imgUrl: '/assets/images/addKeyword.svg',
+      heading: 'Add Keywords or Social Accounts',
+      desc: 'Setup notifications for your startup name, competitors, events and topics that you are interested in.'
+    },
+    {
+      imgUrl: '/assets/images/selectSource.svg',
+      heading: 'Select Sources',
+      desc: 'Pick from dozens of sources, like Twitter, Facebook, Instagram, Reddit, Blogs, News, Youtube & HackerNews.'
+    },
+    {
+      imgUrl: '/assets/images/getNotified.svg',
+      heading: 'Get Notified',
+      desc: 'Receive Slack or Email notifications, once your topic gets mentioned.'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
