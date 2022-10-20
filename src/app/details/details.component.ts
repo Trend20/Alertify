67c14eeede85b60707e88f990b7f56f31421889b
Trend20 <enockomondi305@gmail.com>
2022-10-20 17:02:10 +0300

    adding details component contents

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
+  heading='Why you should use Notify'
+
+  detailsSectionContent=[
+    {
+      heading: 'Never miss a conversation about your brand',
+      desc: 'Get to see when your company is mentioned on various social media outlets and online',
+      imgUrl: '',
+      btnText: 'Get Started'
+    },
+    {
+      heading: 'Acquire More Customers',
+      desc: 'Be notified in real time when someone is looking for your startup service',
+      imgUrl: '',
+      btnText: 'Get Started'
+    },
+    {
+      heading: 'Stay Ahead of Competition',
+      desc: 'Understand what people think about your competitors & devise a killer competitive Strategy',
+      imgUrl: '',
+      btnText: 'Get Started'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
