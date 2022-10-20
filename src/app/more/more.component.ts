73871012c0a993d103548ae0981b988bd5563af5
Trend20 <enockomondi305@gmail.com>
2022-10-20 22:27:06 +0300

    more section

--- src/app/more/more.component.ts
+++ src/app/more/more.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoreComponent implements OnInit {
 
+  moreSectionContent=[
+    {
+      heading: 'Follow Your Industry',
+      desc: 'Are you in a rapidly changing field? stay on top of the newest trends in your industry.',
+      imgUrl: '/assets/images/family.svg',
+    },
+    {
+      heading: 'Find Press Coverage',
+      desc: 'Find news articles, blogs & tweets about your startup & build relationships with authors.',
+      imgUrl: '/assets/images/b-meeting.svg',
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
