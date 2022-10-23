b9d5d295091aab70b869c3a05d329ea5dfcf4cc7
Trend20 <enockomondi305@gmail.com>
2022-10-23 11:45:39 +0300

    card styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +15,7 @@ import { HeroComponent } from './hero/hero.component';
 import { TestimoniesComponent } from './testimonies/testimonies.component';
 import { DetailsComponent } from './details/details.component';
 import { MoreComponent } from './more/more.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -30,7 +33,10 @@ import { MoreComponent } from './more/more.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
