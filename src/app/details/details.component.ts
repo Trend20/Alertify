a7fe74f3f4ddd6ff4e6974313a029c9210f279c7
Trend20 <enockomondi305@gmail.com>
2022-10-23 13:38:50 +0300

    details section styles

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -14,19 +14,22 @@ export class DetailsComponent implements OnInit {
       heading: 'Never miss a conversation about your brand',
       desc: 'Get to see when your company is mentioned on various social media outlets and online',
       imgUrl: '/assets/images/conversationMiss.svg',
-      btnText: 'Get Started'
+      btnText: 'Get Started',
+      id: 'brand'
     },
     {
       heading: 'Acquire More Customers',
       desc: 'Be notified in real time when someone is looking for your startup service',
       imgUrl: '/assets/images/aquireCustomer.svg',
-      btnText: 'Get Started'
+      btnText: 'Get Started',
+      id: 'customer'
     },
     {
       heading: 'Stay Ahead of Competition',
       desc: 'Understand what people think about your competitors & devise a killer competitive Strategy',
       imgUrl: '/assets/images/competition.jpg',
-      btnText: 'Get Started'
+      btnText: 'Get Started',
+      id: 'competition'
     }
   ]
   constructor() { }
